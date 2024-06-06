b516b2f15f0061a3aef0b93630461c77d0bf3ed3
alexey.kuznetsov <alexey.kuznetsov@latoken.com>
2024-06-06 20:42:02 +0300

    added map

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,45 +1,44 @@
-'use client'
+'use client';
 //css
-import './globals.css'
+import './globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 //other
 import React from 'react';
-import Toast from "@/ui/Toast/Toast";
-import {Provider} from "react-redux";
-import store, {persistedStore} from "@/store/store";
-import NavbarTop from "@/components/general/NavbarTop/NavbarTop";
-import {PersistGate} from "redux-persist/integration/react";
-import FooterBottom from "@/components/general/FooterBottom/FooterBottom";
-import TestWebsiteAlert from "@/components/general/TestWebsiteAlert/TestWebsiteAlert";
-import useScrollTop from "@/hooks/useScrollTop";
-import {YMaps} from "@pbe/react-yandex-maps";
+import Toast from '@/ui/Toast/Toast';
+import { Provider } from 'react-redux';
+import store, { persistedStore } from '@/store/store';
+import NavbarTop from '@/components/general/NavbarTop/NavbarTop';
+import { PersistGate } from 'redux-persist/integration/react';
+import FooterBottom from '@/components/general/FooterBottom/FooterBottom';
+import TestWebsiteAlert from '@/components/general/TestWebsiteAlert/TestWebsiteAlert';
+import useScrollTop from '@/hooks/useScrollTop';
+import { YMaps } from '@pbe/react-yandex-maps';
 
 interface IProviders {
-	children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const Providers = ({ children }:IProviders) => {
+const Providers = ({ children }: IProviders) => {
+  useScrollTop();
 
-	useScrollTop()
-
-	return (
-		<Provider store={store}>
-			<PersistGate persistor={persistedStore}>
-				<YMaps>
-					<Toast />
-					{process.env.NEXT_PUBLIC_APP_TEST === "true" && <TestWebsiteAlert />}
-					<NavbarTop />
-					{children}
-					<FooterBottom />
-				</YMaps>
-			</PersistGate>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore}>
+        <YMaps query={{ apikey: 'f1423869-80d1-4c88-8150-8643fdf24b7b' }}>
+          <Toast />
+          {process.env.NEXT_PUBLIC_APP_TEST === 'true' && <TestWebsiteAlert />}
+          <NavbarTop />
+          {children}
+          <FooterBottom />
+        </YMaps>
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default Providers;
---
+++ src/components/delivery-page/ZonesMap/ZonesMap.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { YMaps, Map, Polygon, Placemark } from '@pbe/react-yandex-maps';
+import './ZonesMap.module.css';
+import nnOblast from './nn.geojson.js';
+
+const ZonesMap = () => {
+  const mapState = {
+    center: [56.326887, 44.005986],
+    zoom: 7,
+  };
+
+  const placemarkPosition = [57.147764, 43.803196];
+
+  return (
+    <YMaps query={{ apikey: 'f1423869-80d1-4c88-8150-8643fdf24b7b' }}>
+      <div className='map-container'>
+        <Map defaultState={mapState} width='100%' height='600px'>
+          <Placemark
+            geometry={placemarkPosition}
+            properties={{
+              iconCaption: 'Кирпичный завод Ковернино',
+            }}
+          />
+          <Polygon
+            geometry={nnOblast.geometry.coordinates}
+            onMouseEnter={(e: any) =>
+              e.originalEvent.target.options.set('fillColor', '#00FF00')
+            }
+            onMouseLeave={(e: any) =>
+              e.originalEvent.target.options.set('fillColor', '#00AF00')
+            }
+            properties={{ hintContent: 'Тариф 1' }}
+            modules={['geoObject.addon.hint']}
+            options={{
+              fillColor: '#00AF00',
+              fillOpacity: 0.4,
+              openEmptyHint: true,
+              strokeColor: '#006400',
+              strokeOpacity: 0.9,
+              strokeWidth: 3,
+            }}
+          />
+        </Map>
+      </div>
+    </YMaps>
+  );
+};
+
+export default ZonesMap;

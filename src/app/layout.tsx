e233066c6dd5cb25fb8dd59615ba4ecee2dd57d9
Kirill Dubasov <cergocergo41@gmail.com>
2024-06-15 14:25:08 +0300

    add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Providers from "@/app/providers";
 import type { Metadata } from "next";
 import { APP_TITLE } from "@/constants/general";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: APP_TITLE,
@@ -28,6 +29,30 @@ const RootLayout = ({ children }: IRootLayout) => {
       </head>
       <body>
         <Providers>{children}</Providers>
+
+        <Script id="yabdex-metrika" type="text/javascript">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(97580805, "init", {
+               clickmap:true,
+               trackLinks:true,
+               accurateTrackBounce:true
+            });`}
+        </Script>
+
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/97580805"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );

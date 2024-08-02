b5878dcb4650c44499f5d5e98b3725904ea930b8
alexey.kuznetsov <alexey.kuznetsov@latoken.com>
2024-08-02 15:02:23 +0300

    meta

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,23 +1,23 @@
-'use client';
+"use client";
 //css
-import './globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 //other
-import React from 'react';
-import Toast from '@/ui/Toast/Toast';
-import { Provider } from 'react-redux';
-import store, { persistedStore } from '@/store/store';
-import NavbarTop from '@/components/general/NavbarTop/NavbarTop';
-import { PersistGate } from 'redux-persist/integration/react';
-import FooterBottom from '@/components/general/FooterBottom/FooterBottom';
-import TestWebsiteAlert from '@/components/general/TestWebsiteAlert/TestWebsiteAlert';
-import useScrollTop from '@/hooks/useScrollTop';
-import { YMaps } from '@pbe/react-yandex-maps';
+import React from "react";
+import Toast from "@/ui/Toast/Toast";
+import { Provider } from "react-redux";
+import store, { persistedStore } from "@/store/store";
+import NavbarTop from "@/components/general/NavbarTop/NavbarTop";
+import { PersistGate } from "redux-persist/integration/react";
+import FooterBottom from "@/components/general/FooterBottom/FooterBottom";
+import TestWebsiteAlert from "@/components/general/TestWebsiteAlert/TestWebsiteAlert";
+import useScrollTop from "@/hooks/useScrollTop";
+import { YMaps } from "@pbe/react-yandex-maps";
 
 interface IProviders {
   children: React.ReactNode;
@@ -29,27 +29,28 @@ const Providers = ({ children }: IProviders) => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistedStore}>
-        <YMaps query={{ apikey: 'f1423869-80d1-4c88-8150-8643fdf24b7b' }}>
-          <h1 hidden>Купить кирпич в Нижнем Новгороде - Завод Ковернино</h1>
+        <YMaps query={{ apikey: "f1423869-80d1-4c88-8150-8643fdf24b7b" }}>
           <h1 hidden>
-            Купить кирпич в Нижнем Новгороде и Нижегородской области
+            Купить кирпич Нижний Новогород красный кирпич, красный керамический
+            кирпич, облицовочный кирпич, рядовой кирпич Нижний Новгород,
+            Нижегородская область, Арзамас, Балахна, Богородск, Бор, Выкса,
+            Городец, Дзержинск, Заволжье, Кстово, Павлово, Саров
           </h1>
-          <h2 hidden>
-            Купить красный кирпич, красный керамический кирпич, облицовочный
-            кирпич, рядовой кирпич в Нижнем Новгороде и Нижегородской области
-          </h2>
+          <h1 hidden>Купить кирпич в Нижнем Новгороде - Завод Ковернино</h1>
           <h3 hidden>
-            Доставка кирпича Нижний Новгород, Нижегородская область, Арзамас,
-            Балахна, Богородск, Бор, Выкса, Городец, Дзержинск, Заволжье,
-            Кстово, Павлово, Саров
+            красный кирпич, красный керамический кирпич, облицовочный кирпич,
+            рядовой кирпич, одинарный кирпич, полуторный кирпич, двойной кирпич
+            Нижний Новгород
           </h3>
           <p hidden>
-            Лучшие цены на кирпич от производителя в Нижнем Новгороде и
-            Нижегородской области
+            Кирпич от производителя по лучшим ценам в Нижнем Новгороде
           </p>
-          <p hidden>Кирпичный завод Ковернино, Нижегородская область</p>
+          <p hidden>Кирпич Ковернино завод Нижегородская область</p>
+          <p hidden>
+            Кирпич от производителя по лучшим ценам Нижегородская область
+          </p>
           <Toast />
-          {process.env.NEXT_PUBLIC_APP_TEST === 'true' && <TestWebsiteAlert />}
+          {process.env.NEXT_PUBLIC_APP_TEST === "true" && <TestWebsiteAlert />}
           <NavbarTop />
           {children}
           <FooterBottom />

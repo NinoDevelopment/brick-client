8e34c90bffb1585b3b09fa4d0d626cdbd641b9f4
alexey.kuznetsov <alexey.kuznetsov@latoken.com>
2024-08-23 10:46:42 +0300

    added gallery meta

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import GalleryPage from "@/pagesList/GalleryPage/GalleryPage";
 import { Metadata } from "next";
-import { META_CONTACTS } from "@/constants/metadata";
+import { META_GALLERY } from "@/constants/metadata";
 
-export const metadata: Metadata = META_CONTACTS;
+export const metadata: Metadata = META_GALLERY;
 
 const page = () => <GalleryPage />;
 
